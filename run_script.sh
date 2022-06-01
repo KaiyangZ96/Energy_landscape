1aa3b11d4d21fa73f634b122c513ccd323c9bb68
zhangkaiyang <zhangkaiyang96@gmail.com>
2022-06-01 17:10:16 +0900

    update

--- run_script.sh
+++ run_script.sh
@@ -1,2 +1,2 @@
-python excute.py --target_path '/Users/zhangkaiyang/Documents/Projects/Small_practices/tem2' \
---binarized_file 'binarized_data_CD.txt' --save_path '/Users/zhangkaiyang/Documents/Projects/Small_practices/tem'
+python excute.py --target_path '' \
+--binarized_file '' --save_path ''
--- run_script_docker.sh
+++ run_script_docker.sh
@@ -1,6 +1,6 @@
 SHELL_FOLDER=$(cd "$(dirname "$0")"; pwd);
 TARGET_FOLDER=${1:-"$SHELL_FOLDER"} # the path of binary.txt and rownames.txt
 RESULTS_FOLDER=${2:-"$SHELL_FOLDER"} #the path to save result files
-BINARY_FILENAME=${3:-"binary.txt"} # the exact name of binary file
+BINARY_FILENAME=${3:-"binary.txt"} # the exact name of the binary file
 docker run -itd -v "$TARGET_FOLDER":/usr/target_folder -v "$RESULTS_FOLDER":/usr/results_folder kaiyangz96/energy_landscape:2.0 python3.9 /usr/Scripts_EL/excute.py --target_path /usr/target_folder --binarized_file $BINARY_FILENAME --save_path /usr/results_folder;
 echo -e " running energy landscape on target path $TARGET_FOLDER \n the reults will be saved to path $RESULTS_FOLDER"; 
